5244b750b79663496395bb2fe404e0f29631cb89
Kazuki <96904689+Kazuki123-k@users.noreply.github.com>
2022-12-02 14:15:38 +0900

    feat: fix try catch error

--- server/routes/game.controller.ts
+++ server/routes/game.controller.ts
@@ -4,24 +4,31 @@ import GameSchema from "../routes/game.model";
 
 //get request
 const getAll = async (req: Request, res: Response) => {
-  console.log("this is running");
-  const allList = await GameSchema.find();
-  res.status(200).json({ success: true, data: allList });
+  try {
+    const allList = await GameSchema.find();
+    res.status(200).json({ success: true, data: allList });
+  } catch (err) {
+    res.status(409).send(err);
+  }
 };
 
 //post request
 const postGame = async (req: Request, res: Response) => {
   //console.log(GameSchema);
   let { titleOfGame, description, uId, isPrivate, gameModules } = req.body;
-  const newGame = new GameSchema({
-    titleOfGame: titleOfGame,
-    description: description,
-    uId: uId,
-    isPrivate: isPrivate,
-    gameModules: gameModules,
-  });
-  const save = await newGame.save();
-  res.status(201).json({ success: true, data: save });
+  try {
+    const newGame = new GameSchema({
+      titleOfGame: titleOfGame,
+      description: description,
+      uId: uId,
+      isPrivate: isPrivate,
+      gameModules: gameModules,
+    });
+    const save = await newGame.save();
+    res.status(201).json({ success: true, data: save });
+  } catch (err) {
+    res.status(401).send("This is erroring");
+  }
 };
 
 module.exports = { postGame, getAll };
